b066f1612cdfc73539fabd56df24016927b2e15d
Heyb Padao <heybpadao19@gmail.com>
2021-12-14 16:57:48 +0800

    new file:   css/style.css 	deleted:    css/uikit-rtl.css 	deleted:    css/uikit-rtl.min.css 	deleted:    css/uikit.css 	deleted:    css/uikit.min.css 	modified:   home.php 	new file:   img/banner.jpg 	new file:   img/banner2.jpg 	new file:   img/icon/Twitter social icons - circle - white.png 	new file:   img/icon/f_logo_RGB-White_58.png

--- home.php
+++ home.php
@@ -4,24 +4,63 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style/style.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto&display=swap" rel="stylesheet">
-    <title>DCGS Portal</title>
+    <link href="css/style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/1acd2c2806.js" crossorigin="anonymous"></script>
+    <title>DCGS</title>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&family=Luxurious+Script&family=Pacifico&family=Righteous&display=swap');
+    .index-banner {
+        background-image: url('img/banner.jpg');
+        background-repeat: no-repeat;
+        background-position: bottom;
+        background-size: cover;
+    }
+    </style>
+
 </head>
 <body>
     <header>
-        <div class="logo">
-            <a href="https://www.davaocity.gov.ph/" target="_blank" rel="noopener noreferrer"><img src="imgs/city-gov-davao-logo.png" alt="Go to City Government of Davao webpage..."></a>
-            <a href="home.php">DCGS Portal</a>
-            <p>Davao City Government Services Portal</p>
-        </div>
+        <a href="home.php" class="header-name">CGDeS Portal</a>
+        <nav>
+            <ul>
+                <li class="border-right"><a href="#">optional</a></li>
+                <li class="border-right"><a href="#">menu</a></li>
+                <li><a href="#">here</a></li>
+            </ul>
+        </nav>
     </header>
     <main>
+        <section class="index-banner">
+            <div class="vertical-center">
+                <h1>City Government of Davao</h1>
+                <h2>eServices Portal</h2>
+            </div>
+        </section>
+        <section class="index-links">
+            <a href="#">
+                <div class="index-button">
+                    <h3>Document Search</h3>
+                </div>
+            </a>
+            <a href="#">
+                <div class="index-button">
+                    <h3>Request Certification</h3>
+                </div>
+            </a>
+            <a href="#">
+                <div class="index-button">
+                    <h3>CGD eServices</h3>
+                </div>
+            </a>
+        </section>
+
+
+
         <!--<section class="index-text">
             <div class="vert-center">
                 <h2>Where do you want to go?</h2>
             </div>
-        </section>-->
+        </section>
         <section class="index-banner">
             <h1>HEADER STUFF HERE</h1>
             <p>Some discription below the header to further explain the system...</p>
@@ -46,9 +85,14 @@
                 </div>
             </a>
         </section>
-    </main>
+
+    </main>-->
     <footer>
-        <p>Copyrights chuchu and stuffs</p>
+        <ul>
+            <li><a href="https://www.davaocity.gov.ph/"><i class="fas fa-globe"></i>davaocity.gov.ph</a></li>
+            <li><a href="https://www.facebook.com/davaocitygov"><i class="fab fa-facebook"></i>facebook.com/davaocitygov</a></li>
+            <li><a href="https://twitter.com/davaocitygov"><i class="fab fa-twitter"></i>twitter.com/davaocitygov</a></li>
+        </ul>
     </footer>
 </body>
 </html>
